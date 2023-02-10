f93ca19f6bb25eb4fc649676067931101e97015a
Tony Allen <tony@allen.gg>
2023-02-09 17:30:28 -0800

    compression builds

--- chronosphere/src/filters/network/stream_compressor/stream_compressor.cc
+++ chronosphere/src/filters/network/stream_compressor/stream_compressor.cc
@@ -5,13 +5,83 @@
 
 #include "source/common/common/assert.h"
 
+#include "zstd.h"
+
 namespace Envoy {
 namespace Filter {
 
-Network::FilterStatus StreamCompressorFilter::onData(Buffer::Instance& data, bool) {
-  ENVOY_CONN_LOG(trace, "echo: got {} bytes", read_callbacks_->connection(), data.length());
-  read_callbacks_->connection().write(data, false);
-  return Network::FilterStatus::StopIteration;
+Network::FilterStatus StreamCompressorFilter::onNewConnection() {
+  compression_ctx_ = ZSTD_createCCtx();
+  if (compression_ctx_ == nullptr) {
+    ENVOY_CONN_LOG(error, "failed to create compression context, bypassing compression", read_callbacks_->connection());
+    bypass_ = true;
+    return Network::FilterStatus::Continue;
+  }
+
+  input_buf_.resize(ZSTD_CStreamInSize());
+  output_buf_.resize(ZSTD_CStreamOutSize());
+  return Network::FilterStatus::Continue;
+}
+
+Network::FilterStatus StreamCompressorFilter::onData(Buffer::Instance& data, bool end_stream) {
+  ZSTD_inBuffer zbuf_in;
+  ZSTD_outBuffer zbuf_out;
+
+  if (bypass_) {
+    read_callbacks_->connection().write(data, end_stream);
+    return Network::FilterStatus::Continue;
+  }
+
+  // Iterate through the constituent slices of the data and feed them into the
+  // compressor.
+  const auto uncompressed_size = data.length();
+  auto raw_slices = data.getRawSlices();
+  for (size_t i = 0; i < raw_slices.size(); i++) {
+    auto slice = raw_slices[i];
+    zbuf_in.src = slice.mem_;
+    zbuf_in.size = slice.len_; 
+    zbuf_in.pos = 0;
+    zbuf_out.dst = output_buf_.data();
+    zbuf_out.size = output_buf_.size(); 
+    zbuf_out.pos = 0;
+
+    ZSTD_EndDirective mode;
+    const bool last_slice = (i == raw_slices.size() - 1);
+    while (zbuf_in.pos != zbuf_in.size) {
+      if (last_slice) {
+        // Since there are no more slices, we want to conclude the block.
+        // However, if the data stream is not concluded, we don't want to end
+        // the zstd stream, so we want to use `ZSTD_e_flush` in that case.
+        // Doing so will allow future data to reference previously compressed
+        // data and improve the compression ratio, whereas `ZSTD_e_end` will
+        // reset the context.
+        mode = end_stream ? ZSTD_e_end : ZSTD_e_flush;
+      } else {
+        mode = ZSTD_e_continue;
+      }
+
+      auto remaining = ZSTD_compressStream2(compression_ctx_, &zbuf_out, &zbuf_in, mode);
+      if (ZSTD_isError(remaining)) {
+        // There's no coming back from an error like this. Kill the connection.
+        ENVOY_CONN_LOG(error, "failed to compress data with error {}", read_callbacks_->connection(), remaining);
+        read_callbacks_->connection().close(Network::ConnectionCloseType::NoFlush);
+        return Network::FilterStatus::StopIteration;
+      }
+    }
+
+    data.add(zbuf_out.dst, zbuf_out.pos);
+  }
+
+  // Discard the unneeded and uncompressed data, leaving only the compressed
+  // bytes we want to write to the connection.
+  data.drain(uncompressed_size);
+  read_callbacks_->connection().write(data, end_stream);
+  return Network::FilterStatus::Continue;
+}
+
+Network::FilterStatus StreamCompressorFilter::onWrite(Buffer::Instance& , bool) {
+  // TODO tallen
+  return Network::FilterStatus::Continue;
 }
 
 } // namespace Filter
--- chronosphere/src/filters/network/stream_compressor/stream_compressor.h
+++ chronosphere/src/filters/network/stream_compressor/stream_compressor.h
@@ -1,8 +1,11 @@
 #pragma once
 
-#include "envoy/network/filter.h"
+#include <vector>
 
+#include "envoy/network/filter.h"
 #include "source/common/common/logger.h"
+
+#include "zstd.h"
 
 namespace Envoy {
 namespace Filter {
@@ -10,17 +13,33 @@ namespace Filter {
 /**
  * Implementation of the zstd stream compressor filter.
  */
-class StreamCompressorFilter : public Network::ReadFilter, Logger::Loggable<Logger::Id::filter> {
+class StreamCompressorFilter : public Network::ReadFilter, 
+                               public Network::WriteFilter, 
+                               Logger::Loggable<Logger::Id::filter> {
 public:
+  StreamCompressorFilter();
+  ~StreamCompressorFilter() { ZSTD_freeCCtx(compression_ctx_); }
+
   // Network::ReadFilter
   Network::FilterStatus onData(Buffer::Instance& data, bool end_stream) override;
-  Network::FilterStatus onNewConnection() override { return Network::FilterStatus::Continue; }
+  Network::FilterStatus onNewConnection() override;
   void initializeReadFilterCallbacks(Network::ReadFilterCallbacks& callbacks) override {
     read_callbacks_ = &callbacks;
   }
 
+  // Network::WriteFilter
+  Network::FilterStatus onWrite(Buffer::Instance& data, bool end_stream) override;
+
 private:
   Network::ReadFilterCallbacks* read_callbacks_{};
+
+  ZSTD_CCtx* compression_ctx_;
+
+  // Stores the compressed data.
+  std::vector<char> input_buf_;
+  std::vector<char> output_buf_;
+
+  bool bypass_{};
 };
 
 } // namespace Filter
